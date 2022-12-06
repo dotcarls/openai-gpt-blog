7ff3cced36b7d29f1fcbc4dee2b4802b51f9f4f1
Tim Carlson <tim.carlson@gmail.com>
2022-12-06 09:08:48 -0600

    Commit 11
    Input:
    
    Update the index page to display the list of blog posts using the post-list template
    
    Output:
    
    Here is an updated example of what the pages/index.js file might look like, using the post-list.template file to display the list of blog posts:
    
    ```
    const fs = require('fs');
    const path = require('path');
    
    // Import the layout template
    const layout = fs.readFileSync(path.join(__dirname, '../templates/layout.template'), 'utf8');
    
    // Import the post-list template
    const postList = fs.readFileSync(path.join(__dirname, '../templates/post-list.template'), 'utf8');
    
    // Define the list of blog posts
    const posts = [
      {
        title: 'My First Blog Post',
        author: 'Jane Doe',
        date: 'January 01, 2021',
        summary: 'In this post, I discuss the topic of my first blog post.'
      },
      {
        title: 'My Second Blog Post',
        author: 'Jane Doe',
        date: 'January 02, 2021',
        summary: 'In this post, I discuss the topic of my second blog post.'
      }
    ];
    
    // Define the content for the page
    const content = postList.replace('{{#posts}}', posts.map(post => {
      return `
        <li>
          <h2>${post.title}</h2>
          <p>by ${post.author} on ${post.date}</p>
          <p>${post.summary}</p>
        </li>
      `;
    }).join(''))
    .replace('{{/posts}}', '');
    
    // Render the page
    console.log(layout.replace('{{ content }}', content));
    ```
    
    In this updated example, the pages/index.js file imports the post-list.template file and defines an array of blog posts. The content attribute for

--- blog-website/pages/index.js
+++ blog-website/pages/index.js
@@ -4,15 +4,36 @@ const path = require('path');
 // Import the layout template
 const layout = fs.readFileSync(path.join(__dirname, '../templates/layout.template'), 'utf8');
 
-// Import the post template
-const post = fs.readFileSync(path.join(__dirname, '../templates/post.template'), 'utf8');
+// Import the post-list template
+const postList = fs.readFileSync(path.join(__dirname, '../templates/post-list.template'), 'utf8');
+
+// Define the list of blog posts
+const posts = [
+  {
+    title: 'My First Blog Post',
+    author: 'Jane Doe',
+    date: 'January 01, 2021',
+    summary: 'In this post, I discuss the topic of my first blog post.'
+  },
+  {
+    title: 'My Second Blog Post',
+    author: 'Jane Doe',
+    date: 'January 02, 2021',
+    summary: 'In this post, I discuss the topic of my second blog post.'
+  }
+];
 
 // Define the content for the page
-const content = `
-  <h1>My Blog</h1>
+const content = postList.replace('{{#posts}}', posts.map(post => {
+  return `
+    <li>
+      <h2>${post.title}</h2>
+      <p>by ${post.author} on ${post.date}</p>
+      <p>${post.summary}</p>
+    </li>
+  `;
+}).join(''))
+.replace('{{/posts}}', '');
 
-  ${post.replace('{{ title }}', 'My First Blog Post')
-           .replace('{{ author }}', 'Jane Doe')
-           .replace('{{ date }}', 'January 01, 2021')
-           .replace('{{ tags }}', 'SEO, keywords, blogging')
-           .replace('{{ summary }}',
+// Render the page
+console.log(layout.replace('{{ content }}', content));
